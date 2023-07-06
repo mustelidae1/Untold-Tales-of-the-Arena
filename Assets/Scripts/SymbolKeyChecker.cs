7fcc071cd6fad1f35f1e2dde6d1d31310abc2ccd
mustelidae1 <othm@live.com>
2023-07-06 15:02:50 -0600

    Made P7 only solvable after unlocking the others

--- Assets/Scripts/SymbolKeyChecker.cs
+++ Assets/Scripts/SymbolKeyChecker.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class SymbolKeyChecker : MonoBehaviour
 {
     public SymbolKey symKey;
+    public GameObject reward; 
 
     void Start()
     {
         GameManager.S.showSymKeyButton(false); 
         GameManager.S.updateRewards(symKey);
+        foreach (Transform c in transform)
+        {
+            UnlockableItem u = c.GetComponent<UnlockableItem>();
+            if (u && !u.gameObject.activeSelf) return; 
+        }
+        // all the symbols are activated 
+        reward.SetActive(true); 
     }
 
     void OnDestroy()
